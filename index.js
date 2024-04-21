096dd68df57ae156d0bed586badc9d04c654da90
Burlacu Rodion <burlacurodionmd@gmail.com>
2024-04-21 12:43:12 +0300

    add ex

--- index.js
+++ index.js
@@ -46,4 +46,43 @@ obj3.hobby = 'swimming'
 console.log(obj3)
 //-----------------------------------------//
 
+const obj4 = {
+    name:'Miron'
+}
+Object.preventExtensions(obj4)
+obj4.age = 13
+//console.log(obj4.age) // undefined
+Object.freeze(obj4)
+obj4.name='Andrei'
+console.log(obj4)
+//-----------------------------------------//
 
+function func(list,el) {
+    return list.reduce((acc,curr)=>{
+        if(!acc) return acc;
+        return curr === el
+    },true)
+}
+console.log(func([5,5,5],5))
+//------------------------------------------//
+
+function func1(list,el) {
+    return list.reduce((acc,curr,idx)=>{
+        if(acc !== -1 ) return acc;
+        return curr === el ? idx : -1
+    }, -1)
+}
+
+console.log(func1([6,7,8],4))
+//--------------------------------------------//
+
+function func3(list, el) {
+    return list.reduce((acc, curr)=>{
+        if(curr === el) {
+            acc.push(el)
+        };
+        return acc
+    },[])
+}
+console.log(func3([1,4],2))
+//------------------------------------------//
